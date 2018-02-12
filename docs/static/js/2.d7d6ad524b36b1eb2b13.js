9fbf85d62f51616c937d093890ff45c4dcd401c4
b-jan <bapt.jan@gmail.com>
2018-02-12 08:20:23 +0000

    Build vue js code

---
+++ docs/static/js/2.d7d6ad524b36b1eb2b13.js
@@ -0,0 +1,2 @@
+webpackJsonp([2],{c27y:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var r={render:function(){var e=this.$createElement,t=this._self._c||e;return t("div",[t("o",[this._v("About")])],1)},staticRenderFns:[]},u=n("VU/8")({name:"About"},r,!1,null,null,null);t.default=u.exports}});
+//# sourceMappingURL=2.d7d6ad524b36b1eb2b13.js.map
