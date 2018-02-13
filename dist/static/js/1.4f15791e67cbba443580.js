940380a7f12b92084e25418ccf556e27992e009d
b-jan <bapt.jan@gmail.com>
2018-02-13 21:50:44 +0000

    Create requirements to deploy on Heroku

---
+++ dist/static/js/1.4f15791e67cbba443580.js
@@ -0,0 +1,2 @@
+webpackJsonp([1],{YcJa:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var r={render:function(){var e=this.$createElement,t=this._self._c||e;return t("div",[t("o",[this._v("404 - Not Found")])],1)},staticRenderFns:[]},u=n("VU/8")({name:"NotFound"},r,!1,null,null,null);t.default=u.exports}});
+//# sourceMappingURL=1.4f15791e67cbba443580.js.map
